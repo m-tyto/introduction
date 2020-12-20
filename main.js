1dcc47fd26f6f755ee0033d10449e04bdd16dea8
三宅泰斗 <miyaketaito@miyaketaitonoMacBook-Pro.local>
2020-12-20 21:33:47 +0900

    first commmit

---
+++ main.js
@@ -0,0 +1,22 @@
+'use script'
+
+
+{
+  function loading(){
+    document.getElementById("loading").style.display = "none";
+  }
+
+  setTimeout(loading,5000);
+
+  const pics = ["photo/friend.jpg","photo/sunset.jpeg","photo/hakone.mont.jpeg","photo/isigaki_hotel.JPG","photo/isigaki_sea.JPG","photo/karaokescore.jpeg","photo/masaru.JPG","photo/snowboard.JPG"];
+  let num = 0;
+
+  function slideshow(){
+      num = Math.floor(Math.random()*pics.length);
+      document.querySelector("body").style.backgroundImage = "url(" + pics[num] + ")";
+  }
+
+  setInterval(slideshow, 5000);    
+}
+   
+    
